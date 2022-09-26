b249072cf96959a0bf46e6970c1a8e6a2c7c0269
Antoine <a-guillot@laposte.net>
2022-09-26 16:02:11 +0200

    modification accueil en cours

--- src/components/HomepageButton/index.js
+++ src/components/HomepageButton/index.js
@@ -2,12 +2,17 @@
 // data, styles
 import './style.scss';
 import { Link } from 'react-router-dom';
+import Mascotte from 'src/assets/images/mascotte.gif';
 
 function HomepageButton() {
   return (
-    <div className="QuizzButton">
+    <div className="home">
+      <p className="home-content">Bienvenue sur MNG! Vous êtes en quête de la petite
+        pépite que vous avez loupé? Nous sommes là pour ça!
+      </p>
+      <img className="home-picture" src={Mascotte} alt="mascotte du site" />
       <Link to="/Quizz">
-      <button type="button" className="homepageButton">Questions</button>
+        <button type="button" className="home-button">Commencer le questionnaire</button>
       </Link>
     </div>
   );
