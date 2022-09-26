d9911b1a07df8e4e42f42788672b028df78cbe32
Camille Salomon <lilwayne1923@gmail.com>
2022-09-26 10:51:33 +0200

    création du composant game

---
+++ src/components/Game/index.js
@@ -0,0 +1,15 @@
+// == Import
+// composants
+// data, styles
+import './style.scss';
+
+function Game() {
+  const { slug } = 
+  return (
+    <div className="game">
+      <p>yo</p>
+    </div>
+  );
+}
+
+export default Game;
