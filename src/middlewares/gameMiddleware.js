0311748c2d84e2adfbcea3c57f190713c073c9fe
Camille Salomon <lilwayne1923@gmail.com>
2022-09-29 14:22:19 +0200

    token ajouté au gameMiddleware dynamiquement

--- src/middlewares/gameMiddleware.js
+++ src/middlewares/gameMiddleware.js
@@ -6,12 +6,16 @@ const gameMiddleware = (store) => (next) => (action) => {
     case DISPLAY_FIRST_QUESTION:
       axios.get(
         'http://cyonefr-server.eddi.cloud/api/quiz',
+        {
+          headers: { Authorization: `Bearer ${store.getState().user.token}` },
+        },
       )
         .then((response) => {
-          console.log(response);
+          console.log(store.getState().user.token);
         })
         .catch((error) => {
           console.log(error);
+          console.log(store.getState().user.token);
         });
 
       break;
